c92be42e2067059cc0e4edb2d3d352d823d7c86b
Neeraj Patil <patilneeraj2212@gmail.com>
2020-11-15 21:49:43 +0530

    React-native component 2

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -5,21 +5,22 @@ import { Avatar, ListItem } from "react-native-elements";
 function Menu(props) {
 	const renderMenuItem = ({ item, index }) => {
 		return (
-			<ListItem key={index}>
-				<Avatar rounded source={{ uri: require("./images/uthappizza.png") }} />
+			<ListItem key={index} onPress={() => props.onPress(item.id)}>
+				<Avatar rounded source={require("./images/uthappizza.png")} />
 				<ListItem.Content>
 					<ListItem.Title>{item.name}</ListItem.Title>
 					<ListItem.Subtitle>{item.description}</ListItem.Subtitle>
 				</ListItem.Content>
 				<ListItem.Chevron />
 			</ListItem>
-			// <ListItem
-			// 	key={index}
-			// 	title={item.name}
-			// 	subtitle={item.description}
-			// 	hideChevron={true}
-			// 	leftAvatar= {{ source: require('./images/uthappizza.png')}}
-			// />
+			//  <ListItem
+			//               key={index}
+			//               title={item.name}
+			//               subtitle={item.description}
+			//               hideChevron={true}
+			//               onPress={() => props.onPress(item.id)}
+			//               leftAvatar={{ source: require('./images/uthappizza.png')}}
+			//             />
 		);
 	};
 
